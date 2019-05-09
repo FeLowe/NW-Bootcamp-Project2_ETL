f7f7dea956601d45b3230e8034da6fdf7e8038a3
Francisco Hohn <francisco.hohn@gmail.com>
2019-05-09 16:03:36 -0500

    sucessfully uploaded country data to db

--- ETL_WineClimate.sql
+++ ETL_WineClimate.sql
@@ -5,28 +5,31 @@ use wine_climate_db;
 create table wines (
 	id int primary key,
     country varchar(50),
-    province varchar(50),
-    wine int,
+    designation varchar(100),
     score int,
+    price float,
     variety varchar(50)
 );
 
-/* if needed, code to drop table:
+/*
+if needed, code to drop table:
 drop table wines;
 */
 
 create table country_climates(
 	id int primary key,
     country varchar(50),
-    province varchar(50),
     climate varchar(50)
 );
 
-/* if needed, code to drop table:
+/*
+if needed, code to drop table:
 drop table country_climates;
 */
 
-select w.country, w.province, wine, score, variety, c.climate
+select * from country_climates;
+
+select w.country, designation, score, variety, c.climate
 from wines as w
 join country_climates as c
-on (w.country = c.country and w.province = c.province);
+on (w.country = c.country);
